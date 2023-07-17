c73e197ec46a085cbf05b6f097d46db7efc7f000
Razeenofficial <100760362+Razeenofficial@users.noreply.github.com>
2023-07-17 21:16:52 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,7 +59,7 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Portfolio | Razeen Sable";
+            document.title = "Portfolio | Razeen ";
             $("#favicon").attr("href", "assets/images/favicon.png");
         }
         else {
